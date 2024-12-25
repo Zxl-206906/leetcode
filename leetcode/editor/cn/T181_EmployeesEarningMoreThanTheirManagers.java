2f9943a83d28818e38704081fdc1819db2d46076
Zhang Xilong <2069065992@qq.com>
2024-12-25 18:41:04 +0800

    2024.12.25  - T181_EmployeesEarningMoreThanTheirManagers 超过经理的收入

---
+++ leetcode/editor/cn/T181_EmployeesEarningMoreThanTheirManagers.java
@@ -0,0 +1,13 @@
+package leetcode.editor.cn;
+
+// Java: 超过经理收入的员工
+
+public class T181_EmployeesEarningMoreThanTheirManagers {
+    public static void main(String[] args) {
+//        Solution solution = new T181_EmployeesEarningMoreThanTheirManagers().new Solution();
+        // TO TEST
+        
+    }
+
+    //There is no code of Java type for this problem
+}
