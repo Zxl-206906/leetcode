36cc2f1e121cfa6e826e439237b53f1bddb1cba0
Zhang Xilong <2069065992@qq.com>
2024-12-30 11:37:41 +0800

    2024.12.30  - T193_ValidPhoneNumbers 有效电话号码

---
+++ leetcode/editor/cn/T193_ValidPhoneNumbers.java
@@ -0,0 +1,13 @@
+package leetcode.editor.cn;
+
+// Java: 有效电话号码
+
+public class T193_ValidPhoneNumbers {
+    public static void main(String[] args) {
+        //Solution solution = new T193_ValidPhoneNumbers().new Solution();
+        // TO TEST
+        
+    }
+
+    //There is no code of Java type for this problem
+}
